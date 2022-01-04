11660430770549309d5b9685e7ad1e161f5e10bc
Nametsego <Nametsegothakadu@gmail.com>
2022-01-04 05:27:10 +0200

    Initial Commit

---
+++ src/AntiElite/com/Main.java
@@ -0,0 +1,10 @@
+package AntiElite.com;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"ThiS is more random info :D","title", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
